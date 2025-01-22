3836d11768cecc7ae7e7b0e96bdc6983ce70e9c2
Hannahjjj97 <hannahjjj97@gmail.com>
2025-01-23 06:20:05 +0800

    feat: PID 增加了 setsetpoint method、增加了 PID 控制 UP

--- src/main/java/frc/robot/ClimberSubsystem.java
+++ src/main/java/frc/robot/ClimberSubsystem.java
@@ -5,46 +5,49 @@
 package frc.robot;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimberSubsystem extends SubsystemBase {
-private final VictorSP climbermotor;
-private final PIDController climberPID;
+  private final VictorSP climbermotor;
+  private final PIDController climberPID;
+  private final Encoder climberEncoder;
 
   /** Creates a new ClimberSubsystem. */
   public ClimberSubsystem() {
-  climbermotor = new VictorSP(43);
-  climberPID = new PIDController(0.08, 0, 0);
+    climbermotor = new VictorSP(43);
+    climberPID = new PIDController(0.08, 0, 0);
+    climberEncoder = new Encoder(0, 0);
   }
 
-  private void climbUp (){
-  
+  private void climbUp() {
+    climbermotor.set(climberPID.calculate(climberEncoder.get()));
   }
 
- 
-
-  private void climbDown(){
-  climbermotor.set(-0.35);
+  private void setsetpoint() {
+    climberPID.setSetpoint(1);
   }
 
-  private double getSetpoint(){
+  private void climbDown() {
+    climbermotor.set(-0.35);
+  }
+
+  private double getSetpoint() {
     return climberPID.getSetpoint();
   }
-  
 
-
-  public Command climbUpcmd(){
-  Command cmd = run(this::climbUp);
-  return cmd;
+  public Command climbUpcmd() {
+    Command cmd = run(this::climbUp);
+    return cmd;
   }
 
-  public Command climbDowncmd(){
+  public Command climbDowncmd() {
     Command cmd = run(this::climbDown);
     return cmd;
   }
-  
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
