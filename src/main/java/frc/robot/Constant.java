63e6873862bd1bb8893abed59915958e4151edce
capy0827 <j0921651289@gmail.com>
2025-01-22 18:08:53 +0800

    feat:Ramp 把setSspeed改成setVoltage

---
+++ src/main/java/frc/robot/Constant.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+public final class Constant {
+
+    public static class RampConstant {
+        public static final int kUpRampMotor = 6;
+        public static final int kDownRampMotor = 6;
+    }
+
+    public static class CoralShooter {
+        public static final int kOnboard = 0 ; //(要跟電機確認)(DistanceSensor)
+        public static final double kDistanceRange = 4 ; //(DistanceSensor距離)
+	    public static final int kShooterMotorChannel = 1 ; //(要跟電機確定)
+        public static final double kShooterMotorVoltage = 4 ; //(要跟電機確定)
+    }
+    
+}
