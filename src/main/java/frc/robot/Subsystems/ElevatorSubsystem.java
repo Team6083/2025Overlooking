63d0d5e96f4406876ab26d41f22c53e2fd24b8c0
hehongyi0217 <hehongyi0217@gmail.com>
2025-01-23 19:08:16 +0800

    feat:ElevatorSubsystem

---
+++ src/main/java/frc/robot/Subsystems/ElevatorSubsystem.java
@@ -0,0 +1,52 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ElevatorSubsystem extends SubsystemBase {
+  /** Creates a new ElevatorSubsystem. */
+ SparkMax max;
+
+
+   
+
+  public ElevatorSubsystem() {
+    // ElevatorMotor1 = new VictorSP(0);
+    // ElevatorMotor2 = new VictorSP(2);
+    
+}
+
+  public void getSetPoint() {
+
+  }
+  public void ElevatorPID(){
+
+  }
+
+  public void ToTheSecFloor() {
+
+  }
+
+  public void ToTheTrdFloor() {
+
+  }
+
+  public void TotheFourFloor() {
+
+  }
+
+  public void stopMove() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
