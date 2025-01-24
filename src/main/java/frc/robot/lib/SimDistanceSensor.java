b421e2caab867633476caed7d83613dac80b4d98
Frank <s1310266@my.cmsh.cyc.edu.tw>
2025-01-24 11:34:50 +0800

    feat : CoralShooter add CoralShooterSubsystem add CoralShooterInWithAutoStop add DistanceSensor libs

---
+++ src/main/java/frc/robot/lib/SimDistanceSensor.java
@@ -0,0 +1,24 @@
+package frc.robot.lib;
+
+public class SimDistanceSensor implements DistanceSensorInterface {
+
+    public SimDistanceSensor(){
+        super();
+    }
+
+    @Override
+    public double getTargetDistance() {
+        return -1;
+    }
+
+    @Override
+    public boolean isGetTarget() {
+       return true;
+    }
+
+    @Override
+    public void setAutomaticMode(boolean isEnable) {
+        return;
+    }
+    
+}
