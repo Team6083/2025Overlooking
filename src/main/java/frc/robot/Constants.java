e6578a52a592ef59061be443b0b0f06696f812e2
Hannahjjj97 <hannahjjj97@gmail.com>
2025-01-26 00:33:15 +0800

    style: ModifierOrder Unify first "final" and then "static"

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,24 +3,24 @@ package frc.robot;
 public class Constants {
 
   public final static class ClimberConstant {
-    public static final int kClimberSetpoint = 40;
-    public static final double kClimbDownSpeed = -0.35;
+    public final static int kClimberSetpoint = 40;
+    public final static double kClimbDownSpeed = -0.35;
   }
 
   public final static class CoralShooterConstant {
-    public static final int kOnboard = 0;
-    public static final double kDistanceRange = 4;
-    public static final int kShooterMotorChannel = 1;
-    public static final double kShooterMotorSpeed = 4;
+    public final static int kOnboard = 0;
+    public final static double kDistanceRange = 4;
+    public final static int kShooterMotorChannel = 1;
+    public final static double kShooterMotorSpeed = 4;
   }
 
   public final static class AlgaeIntakeConstant {
-    public static final int kIntakeMotorChannel = 2;
-    public static final int kRotateMotorChannel = 1;
-    public static final double kIntakeVoltage = 6.0;
-    public static final double kReIntakeVoltage = 3.0;
-    public static final double kUpIntakeVoltage = 12.0;
-    public static final double kDownIntakeVoltage = -12.0;
+    public final static int kIntakeMotorChannel = 2;
+    public final static int kRotateMotorChannel = 1;
+    public final static double kIntakeVoltage = 6.0;
+    public final static double kReIntakeVoltage = 3.0;
+    public final static double kUpIntakeVoltage = 12.0;
+    public final static double kDownIntakeVoltage = -12.0;
   }
 
 }
