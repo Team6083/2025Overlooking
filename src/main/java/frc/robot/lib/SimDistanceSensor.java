1abd3e80270b8ed3f30c01c43dbef9781038fc46
Hannahjjj97 <hannahjjj97@gmail.com>
2025-01-26 00:27:10 +0800

    style: Indentation 4 space indent ->  2 space indent

--- src/main/java/frc/robot/lib/SimDistanceSensor.java
+++ src/main/java/frc/robot/lib/SimDistanceSensor.java
@@ -2,23 +2,23 @@ package frc.robot.lib;
 
 public class SimDistanceSensor implements DistanceSensorInterface {
 
-    public SimDistanceSensor() {
-        super();
-    }
+  public SimDistanceSensor() {
+    super();
+  }
 
-    @Override
-    public double getTargetDistance() {
-        return -1;
-    }
+  @Override
+  public double getTargetDistance() {
+    return -1;
+  }
 
-    @Override
-    public boolean isGetTarget() {
-        return true;
-    }
+  @Override
+  public boolean isGetTarget() {
+    return true;
+  }
 
-    @Override
-    public void setAutomaticMode(boolean isEnable) {
-        return;
-    }
+  @Override
+  public void setAutomaticMode(boolean isEnable) {
+    return;
+  }
 
 }
