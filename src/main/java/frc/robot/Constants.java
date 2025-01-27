5926eb40775e21e6a64310b1dcb87c52a381cbdc
imnienting <s1310267@my.cmsh.cyc.edu.tw>
2025-01-27 19:51:29 +0800

    fix: addInverted

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
   public static final class ClimberConstant {
     public static final int kClimberSetpoint = 40;
     public static final double kClimbDownSpeed = -0.35;
+    public static final Boolean kclimberMotorInverted = false;
+    public static final Boolean kclimberEncoderInverted = false;
   }
 
   public static final class CoralShooterConstant {
@@ -16,13 +18,13 @@ public class Constants {
 
   public static final class AlgaeIntakeConstant {
     public static final int kIntakeMotorChannel = 2;
-    public static final int kRotateMotorChannel = 1;
     public static final double kIntakeVoltage = 6.0;
     public static final double kReIntakeVoltage = 3.0;
+    public static final Boolean kIntakeMotorInverted = false;
+    public static final int kRotateMotorChannel = 1;
     public static final double kUpIntakeVoltage = 12.0;
     public static final double kDownIntakeVoltage = -12.0;
   }
-
   public static final class PowerDistributionConstant {
     // Motor channel
     public static final int kCoralShooterRightMotorCurrentChannel = 7;
