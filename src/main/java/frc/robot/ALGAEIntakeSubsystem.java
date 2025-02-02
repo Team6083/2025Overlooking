5eeaab6ed3f890229d2d58334d1297fc47619769
imnienting <s1310267@my.cmsh.cyc.edu.tw>
2025-02-02 15:04:23 +0800

    fix: AlgaeIntake 把 Timer 的東西先刪除

--- src/main/java/frc/robot/ALGAEIntakeSubsystem.java
+++ src/main/java/frc/robot/ALGAEIntakeSubsystem.java
@@ -4,7 +4,8 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -12,76 +13,83 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ALGAEIntakeSubsystem extends SubsystemBase {
   /** Creates a new ALGAEIntakeSubsystem. */
-  private final VictorSP intakeMotor; 
+  private final VictorSP intakeMotor;
   private final VictorSP RotateIntakeMotor;
-  private final Timer timer;
+  private final PIDController algaeMotorUpPID;
+  private final PIDController algaeMotorDownPID;
+  private final Encoder algaeEncoder;
+
   public ALGAEIntakeSubsystem() {
 
     intakeMotor = new VictorSP(0);
     RotateIntakeMotor = new VictorSP(1);
-    timer = new Timer();
+    algaeMotorUpPID = new PIDController(0.8, 0, 0);
+    algaeMotorDownPID = new PIDController(0.8, 0, 0);
+    algaeEncoder = new Encoder(1, 0);
   }
 
-  public void setTimer(){
-    timer.start();
-    if (timer.get()>8){
-      intakeMotor.setVoltage(0);
-    }
-  }
-   
-  public void setIntakeMotor(){
-      intakeMotor.setVoltage(6.0);
+  public void setIntakeMotor() {
+    intakeMotor.setVoltage(6.0);
   }
 
-  public void setReIntake(){
-    intakeMotor.setVoltage(3);
+  public void setReIntake() {
+    intakeMotor.setVoltage(-3);
   }
 
-  public void setUpIntake(){
-    RotateIntakeMotor.setVoltage(12);
-  }
-
-  public void setDownIntake(){
-    RotateIntakeMotor.setVoltage(-12);
-  }
-  public void stopRotateIntakeMotor(){
+  public void stopRotateIntakeMotor() {
     RotateIntakeMotor.setVoltage(0);
   }
 
+  public void setUpIntake() {
+    RotateIntakeMotor.set(algaeMotorUpPID.calculate(algaeEncoder.get()));
+  }
 
-public Command setUpIntakeCmd(){ 
+  public void setDownIntake() {
+    RotateIntakeMotor.set(algaeMotorDownPID.calculate(algaeEncoder.get()));
+  }
+
+  public double getUpIntakeSetpoint() {
+    return algaeMotorUpPID.getSetpoint();
+  }
+
+  public double getDownIntakeSetpoint() {
+    return algaeMotorUpPID.getSetpoint();
+  }
+
+  public void setUpIntakeSetpoint() {
+    algaeMotorUpPID.setSetpoint(130);
+  }
+
+  public void setDownIntakeSetpoint() {
+    algaeMotorDownPID.setSetpoint(35);
+  }
+
+  public double getIntakeMotorRotate() {
+    return algaeEncoder.get();
+  }
+
+  public Command setUpIntakeCmd() {
     Command cmd = runEnd(this::setUpIntake, this::stopRotateIntakeMotor);
     return cmd;
   }
-public Command setDownIntakeCmd(){ 
+
+  public Command setDownIntakeCmd() {
     Command cmd = runEnd(this::setDownIntake, this::stopRotateIntakeMotor);
     return cmd;
   }
-  public Command setIntakeMotorCmd(){
+
+  public Command setIntakeMotorCmd() {
     Command cmd = runEnd(this::setUpIntake, this::stopRotateIntakeMotor);
     return cmd;
   }
 
-public Command setReIntakeMotorCmd(){
+  public Command setReIntakeMotorCmd() {
     Command cmd = runEnd(this::setUpIntake, this::stopRotateIntakeMotor);
     return cmd;
   }
 
-
-
-    
-
-
-
-
-
-      
-    
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-    
 
   }
 }
-
