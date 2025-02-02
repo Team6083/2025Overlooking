8c573f37d7194221b233ca8055b8bd6943b7f425
imnienting <s1310267@my.cmsh.cyc.edu.tw>
2025-02-02 15:44:34 +0800

    feat: addInverted 增加反轉程式

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     public static final int kClimberSetpoint = 40;
     public static final double kClimbDownSpeed = -0.35;
     public static final Boolean kclimberMotorInverted = false;
-    public static final Boolean kclimberEncoderInverted = false;
+    public static final Boolean kclimberEncoderInverted = true;
   }
 
   public static final class CoralShooterConstant {
@@ -14,16 +14,18 @@ public class Constants {
     public static final double kDistanceRange = 4;
     public static final int kShooterMotorChannel = 1;
     public static final double kShooterMotorSpeed = 4;
+    public static final Boolean kcoralShooterMotorInverted = false;
   }
 
   public static final class AlgaeIntakeConstant {
     public static final int kIntakeMotorChannel = 2;
+    public static final int kRotateMotorChannel = 1;
     public static final double kIntakeVoltage = 6.0;
     public static final double kReIntakeVoltage = 3.0;
-    public static final Boolean kIntakeMotorInverted = false;
-    public static final int kRotateMotorChannel = 1;
     public static final double kUpIntakeVoltage = 12.0;
     public static final double kDownIntakeVoltage = -12.0;
+    public static final Boolean kIntakeMotorInverted = false;
+    public static final Boolean krotateIntakeMotorInverted = false;
   }
 
   public static final class RampConstant {
