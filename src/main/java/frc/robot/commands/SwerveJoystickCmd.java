4d71bc2c48234ca966a6612b1802e39b16fec733
SallyLin0907 <ples104013@gmail.com>
2025-02-03 19:56:21 +0800

    style: SwerveJoystickCmd 自動排版、改變數名稱

--- src/main/java/frc/robot/commands/SwerveJoystickCmd.java
+++ src/main/java/frc/robot/commands/SwerveJoystickCmd.java
@@ -13,20 +13,23 @@ import frc.robot.drivebase.SwerveDrive;
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class SwerveJoystickCmd extends Command {
   /** Creates a new SwerveJoystickCmd. */
-  private final SwerveDrive swerveDrive ;
+  private final SwerveDrive swerveDrive;
   private final CommandXboxController main;
-  private final SlewRateLimiter xLimiter;
-  private final SlewRateLimiter yLimiter;
+  private final SlewRateLimiter xlimiter;
+  private final SlewRateLimiter ylimiter;
   private final SlewRateLimiter rotLimiter;
   private final double drivebaseMaxSpeed = DriveBaseConstants.kMaxSpeed;
-  private double xSpeed, ySpeed, rotSpeed, magnification;
-  
+  private double xspeed;
+  private double yspeed;
+  private double rotSpeed;
+  private double magnification;
+
   public SwerveJoystickCmd(SwerveDrive swerveDrive, CommandXboxController main) {
     // Use addRequirements() here to declare subsystem dependencies.
     this.swerveDrive = swerveDrive;
     this.main = main;
-    xLimiter = new SlewRateLimiter(DriveBaseConstants.kXLimiterRateLimit);
-    yLimiter = new SlewRateLimiter(DriveBaseConstants.kYLimiterRateLimit);
+    xlimiter = new SlewRateLimiter(DriveBaseConstants.kXLimiterRateLimit);
+    ylimiter = new SlewRateLimiter(DriveBaseConstants.kYLimiterRateLimit);
     rotLimiter = new SlewRateLimiter(DriveBaseConstants.kRotLimiterRateLimit);
     addRequirements(this.swerveDrive);
   }
@@ -35,27 +38,28 @@ public class SwerveJoystickCmd extends Command {
   @Override
   public void execute() {
     magnification = DriveBaseConstants.kmagnification;
-    if(Math.abs(main.getLeftY())>0.1){
-      xSpeed = -xLimiter.calculate(main.getLeftY()) * drivebaseMaxSpeed * magnification;
-    }else{
-      xSpeed = 0;
+    if (Math.abs(main.getLeftY()) > 0.1) {
+      xspeed = -xlimiter.calculate(main.getLeftY()) * drivebaseMaxSpeed * magnification;
+    } else {
+      xspeed = 0;
     }
-    if(Math.abs(main.getLeftX())>0.1){
-       ySpeed = -yLimiter.calculate(main.getLeftX()) * drivebaseMaxSpeed * magnification;
-    }else{
-      ySpeed = 0;
+    if (Math.abs(main.getLeftX()) > 0.1) {
+      yspeed = -ylimiter.calculate(main.getLeftX()) * drivebaseMaxSpeed * magnification;
+    } else {
+      yspeed = 0;
     }
-    if(Math.abs(main.getRightX())>0.1){
-      rotSpeed = -rotLimiter.calculate(main.getRightX()) * drivebaseMaxSpeed*1.2;
-    }else{
+    if (Math.abs(main.getRightX()) > 0.1) {
+      rotSpeed = -rotLimiter.calculate(main.getRightX()) * drivebaseMaxSpeed * 1.2;
+    } else {
       rotSpeed = 0;
     }
-    swerveDrive.drive(xSpeed, ySpeed, rotSpeed, true);
+    swerveDrive.drive(xspeed, yspeed, rotSpeed, true);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
