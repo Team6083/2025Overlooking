e1b238db0da80a753c417dc849b1b46d61412a55
Hannahjjj97 <hannahjjj97@gmail.com>
2025-02-08 17:39:33 +0800

    style: import stylecheck

--- src/main/java/frc/robot/drivebase/SwerveDrive.java
+++ src/main/java/frc/robot/drivebase/SwerveDrive.java
@@ -1,6 +1,5 @@
 package frc.robot.drivebase;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
--- src/main/java/frc/robot/drivebase/SwerveModule.java
+++ src/main/java/frc/robot/drivebase/SwerveModule.java
@@ -5,13 +5,13 @@ package frc.robot.drivebase;
 
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
-import com.revrobotics.spark.config.SparkBaseConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
 import edu.wpi.first.math.controller.PIDController;
