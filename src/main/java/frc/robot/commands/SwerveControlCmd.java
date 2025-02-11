1da2de36c6909aa7e4013e6e723e11a73063c46a
Hannahjjj97 <hannahjjj97@gmail.com>
2025-02-11 23:11:59 +0800

    style: unused import remove unused import

--- src/main/java/frc/robot/commands/SwerveControlCmd.java
+++ src/main/java/frc/robot/commands/SwerveControlCmd.java
@@ -4,9 +4,6 @@
 
 package frc.robot.commands;
 
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.MetersPerSecond;
-
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
