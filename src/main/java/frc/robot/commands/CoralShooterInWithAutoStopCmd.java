2b4d1f0016f06b251b3581a18d7cf833a7769166
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2025-02-12 23:32:35 +0800

    style: format all files

--- src/main/java/frc/robot/commands/CoralShooterInWithAutoStopCmd.java
+++ src/main/java/frc/robot/commands/CoralShooterInWithAutoStopCmd.java
@@ -19,7 +19,7 @@ public class CoralShooterInWithAutoStopCmd extends Command {
   @Override
   public void initialize() {
   }
-  
+
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
