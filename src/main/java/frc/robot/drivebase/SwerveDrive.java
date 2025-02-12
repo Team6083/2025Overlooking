a6202083fccd15259f5b3d76331fad5d14b3b68f
Hannahjjj97 <hannahjjj97@gmail.com>
2025-02-12 21:47:14 +0800

    style: remove unused thing remove unused import

--- src/main/java/frc/robot/drivebase/SwerveDrive.java
+++ src/main/java/frc/robot/drivebase/SwerveDrive.java
@@ -11,7 +11,6 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
