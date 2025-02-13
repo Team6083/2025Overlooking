4ad10f051011417a8b5df31525c14d08616deed4
Frank <s1310266@my.cmsh.cyc.edu.tw>
2025-02-13 18:55:12 +0800

    feat: CoralShooterIntakeModeCmd add some function to this command

--- src/main/java/frc/robot/commands/CoralShooterIntakeModeCmd.java
+++ src/main/java/frc/robot/commands/CoralShooterIntakeModeCmd.java
@@ -1,33 +1,43 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CoralShooterSubsystem;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class CoralShooterIntakeModeCmd extends Command {
-  /** Creates a new CoralShooterIntakeModeCmd. */
-  public CoralShooterIntakeModeCmd() {
+  /** Creates a new CoralShooterInWithAutoStopCmd. */
+
+  private final CoralShooterSubsystem coralShooterSubsystem;
+
+  public CoralShooterIntakeModeCmd(CoralShooterSubsystem coralShooterSubsystem) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.coralShooterSubsystem = coralShooterSubsystem;
+    addRequirements(this.coralShooterSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
-
+  public void initialize() {
+  }
+  
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    coralShooterSubsystem.coralShooterFastOn();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    coralShooterSubsystem.coralShooterStop();
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return coralShooterSubsystem.isGetTarget();
   }
 }
