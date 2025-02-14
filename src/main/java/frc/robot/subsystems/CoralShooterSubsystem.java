35070cc7d550c2cbbb2f12411da354cc75c86427
Frank <s1310266@my.cmsh.cyc.edu.tw>
2025-02-15 00:53:36 +0800

    style: CoralShooterSubsystem checkstyle

--- src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.Rev2mDistanceSensor;
 import com.revrobotics.Rev2mDistanceSensor.Port;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
