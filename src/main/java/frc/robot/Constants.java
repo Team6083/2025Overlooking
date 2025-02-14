94ddf11ae3bb8d542a9a1082d634b194afd3aef7
Frank <s1310266@my.cmsh.cyc.edu.tw>
2025-02-15 00:34:31 +0800

    value: Constants 改馬達速度和編號

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,18 +9,18 @@ import edu.wpi.first.units.measure.LinearVelocity;
 public class Constants {
   public static final class CoralShooterConstant {
     public static final double kDistanceRange = 4.0;
-    public static final int kShooterLeftMotorChannel = 1;
-    public static final int kShooterRightMotorChannel = 2;
-    public static final double kShooterMotorFastSpeed = 0.3;
-    public static final double kShooterMotorSlowSpeed = 0.8;
+    public static final int kShooterLeftMotorChannel = 31;
+    public static final int kShooterRightMotorChannel = 32;
+    public static final double kShooterMotorFastSpeed = 0.2;
+    public static final double kShooterMotorSlowSpeed = 0.08;
     public static final Boolean kCoralShooterRightMotorInverted = false;
     public static final Boolean kCoralShooterLeftMotorInverted = false;
   }
 
   public static final class PowerDistributionConstant {
     // Motor channel
-    public static final int kCoralShooterRightMotorCurrentChannel = 7;
-    public static final int kCoralShooterLeftMotorCurrentChannel = 7;
+    public static final int kCoralShooterRightMotorCurrentChannel = 0;
+    public static final int kCoralShooterLeftMotorCurrentChannel = 1;
     public static final int kAlgaeIntakeMotorCurrentChannel = 7;
     public static final int kAlgaeRotateMotorCurrentChannel = 7;
     public static final int kClimberMotorCurrentChannel = 7;
