ffce7433fceee75ee67e7308b7060bbd8c9131fd
Hannahjjj97 <hannahjjj97@gmail.com>
2025-02-14 23:51:00 +0800

    style: remove empty line 把改掉別人的東西及不通過 Check 的空行刪了

--- src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.Rev2mDistanceSensor.Port;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CoralShooterConstant;
