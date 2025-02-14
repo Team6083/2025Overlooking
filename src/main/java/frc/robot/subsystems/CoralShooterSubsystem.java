85c9f1757b05fc0ceaa086a8bc3bd1a736203d6d
capy0827 <j0921651289@gmail.com>
2025-02-14 21:35:10 +0800

    fix: CoralShooterSubsystem 修改建構方式

--- src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -58,7 +58,7 @@ public class CoralShooterSubsystem extends SubsystemBase {
 
   public boolean isGetTarget() {
     if (distanceSensor.getRange() <= CoralShooterConstant.kDistanceRange
-        && distanceSensor.getRange() > 0) {
+        && distanceSensor.getRange() > 0 && distanceSensor.getRange() < 4.0) {
       return true;
     }
     return false;
