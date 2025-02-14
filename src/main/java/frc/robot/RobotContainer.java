490104d337a4e0f628f0905cc0813e8cf6003b34
Hannahjjj97 <hannahjjj97@gmail.com>
2025-02-14 23:45:59 +0800

    feat: remove unused thing 把測試用的程式拿掉

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,46 +5,43 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-// import frc.robot.commands.CoralShooterInWithAutoStopCmd;
+import frc.robot.commands.CoralShooterInWithAutoStopCmd;
 import frc.robot.commands.SwerveControlCmd;
 import frc.robot.drivebase.SwerveDrive;
 import frc.robot.lib.PowerDistribution;
-// import frc.robot.subsystems.CoralShooterSubsystem;
+import frc.robot.subsystems.CoralShooterSubsystem;
 
 public class RobotContainer {
-  // private final CoralShooterSubsystem coralShooterSubsystem;
-  // private final SendableChooser<Command> autChooser;
+  private final CoralShooterSubsystem coralShooterSubsystem;
+  private final SendableChooser<Command> autChooser;
   private final SwerveDrive swerveDrive;
   private final SwerveControlCmd swerveJoystickCmd;
   private final CommandXboxController mainController;
   private final PowerDistribution powerDistribution;
 
-  // private final Command intakeCommand;
+  private final Command intakeCommand;
 
   public RobotContainer() {
     powerDistribution = new PowerDistribution();
-    // coralShooterSubsystem = new CoralShooterSubsystem(powerDistribution);
+    coralShooterSubsystem = new CoralShooterSubsystem(powerDistribution);
     swerveDrive = new SwerveDrive();
     mainController = new CommandXboxController(0);
     swerveJoystickCmd = new SwerveControlCmd(swerveDrive, mainController);
 
-    // intakeCommand = new SequentialCommandGroup(
-    // coralShooterSubsystem.coralShooterFastOnCmd().until(coralShooterSubsystem::isGetTarget),
-    // new CoralShooterInWithAutoStopCmd(coralShooterSubsystem));
+    intakeCommand = new SequentialCommandGroup(
+    coralShooterSubsystem.coralShooterFastOnCmd().until(coralShooterSubsystem::isGetTarget),
+    new CoralShooterInWithAutoStopCmd(coralShooterSubsystem));
 
-    // autChooser = AutoBuilder.buildAutoChooser();
-    // autChooser.setDefaultOption("DoNothing", Commands.none());
-    // SmartDashboard.putData("CoralShooterSubsystem", coralShooterSubsystem);
-    // SmartDashboard.putData("AutoChooser", autChooser);
+    autChooser = AutoBuilder.buildAutoChooser();
+    autChooser.setDefaultOption("DoNothing", Commands.none());
+    SmartDashboard.putData("CoralShooterSubsystem", coralShooterSubsystem);
+    SmartDashboard.putData("AutoChooser", autChooser);
     configureBindings();
   }
 
@@ -52,6 +49,7 @@ public class RobotContainer {
     swerveDrive.setDefaultCommand(swerveJoystickCmd);
     mainController.a().whileTrue(swerveDrive.setTurningDegreeCmd(90));
     mainController.b().whileTrue(swerveDrive.setTurningDegreeCmd(0));
+    mainController.back().onTrue(swerveDrive.gyroResetCmd());
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.Rev2mDistanceSensor.Port;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CoralShooterConstant;
@@ -67,11 +66,6 @@ public class CoralShooterSubsystem extends SubsystemBase {
           && distanceSensor.getTargetDistance() > 0;
     }
     return false;
-  }
-
-  @Override
-  public void periodic(){
-    SmartDashboard.putBoolean("isGetCoral", isGetTarget());
   }
 
   public Command coralShooterFastOnCmd() { // Motor on
