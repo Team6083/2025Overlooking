2e64b6ccd7830cf84952e9d7e3a37876206c9c77
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2025-02-15 08:03:13 +0800

    value: Constants 改 CoralShooter 馬達速度

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,7 +15,7 @@ public class Constants {
     public static final int kShooterRightMotorChannel = 32;
     // TODO: 微調
     public static final double kShooterMotorFastSpeed = 0.2;
-    public static final double kShooterMotorSlowSpeed = 0.08;
+    public static final double kShooterMotorSlowSpeed = 0.12;
     public static final Boolean kCoralShooterRightMotorInverted = false;
     public static final Boolean kCoralShooterLeftMotorInverted = false;
   }
