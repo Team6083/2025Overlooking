119ffb4c5e3dc1efbe5f874c2b35426443bdda54
capy0827 <j0921651289@gmail.com>
2025-02-16 15:58:36 +0800

    feat: RobotContainer put the command of TagTracking into RobotContainer set y() at first

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,19 +5,28 @@
 package frc.robot;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.revrobotics.ColorSensorV3.MainControl;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.CoralShooterSubsystem;
+import frc.robot.drivebase.SwerveDrive;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 
 public class RobotContainer {
   private final CoralShooterSubsystem coralShooterSubsystem;
   private final SendableChooser<Command> autChooser;
+  private final SwerveDrive swerveDrive;
+  private final CommandXboxController mainController;
 
   public RobotContainer() {
     coralShooterSubsystem = new CoralShooterSubsystem();
+    swerveDrive = new SwerveDrive();
+    mainController = new CommandXboxController(0);
     autChooser = AutoBuilder.buildAutoChooser();
     autChooser.setDefaultOption("DoNothing", Commands.none());
     SmartDashboard.putData("CoralShooterSubsystem", coralShooterSubsystem);
@@ -26,6 +35,9 @@ public class RobotContainer {
   }
 
   private void configureBindings() {
+    mainController.y().whileTrue(swerveDrive.TagTrackingCmd());
+
+    
   }
 
   public Command getAutonomousCommand() {
