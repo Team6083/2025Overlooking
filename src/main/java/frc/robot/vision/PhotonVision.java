d666d6f4dd849c197b6cbd9815811d9b81c208d8
capy0827 <j0921651289@gmail.com>
2025-02-18 12:53:34 +0800

    style: remove duplicated import 有奇怪的 import 變成兩行一模一樣的

--- src/main/java/frc/robot/vision/PhotonVision.java
+++ src/main/java/frc/robot/vision/PhotonVision.java
@@ -6,7 +6,6 @@ package frc.robot.vision;
 
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.List;
 import org.photonvision.PhotonCamera;
