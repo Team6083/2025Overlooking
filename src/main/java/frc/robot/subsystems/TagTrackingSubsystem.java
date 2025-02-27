4dbc34c754048cbfa197fd6f78c3ba4e37aaf312
capy0827 <j0921651289@gmail.com>
2025-02-27 18:32:22 +0800

    style:checkstyle

--- src/main/java/frc/robot/subsystems/TagTrackingSubsystem.java
+++ src/main/java/frc/robot/subsystems/TagTrackingSubsystem.java
@@ -12,7 +12,6 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import java.io.IOException;
 import java.util.Optional;
 
