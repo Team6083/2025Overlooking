d5ef0e9595677419dbc278361e50e1c517538a68
Hannahjjj97 <hannahjjj97@gmail.com>
2025-02-28 16:18:44 +0800

    fix: update waypoint coordinates for improved path planning and navigation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -23,7 +23,7 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
-    CameraServer.startAutomaticCapture();
+    // CameraServer.startAutomaticCapture();
   }
 
   @Override
