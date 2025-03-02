f8b1a6d53340b842422e920ed7c189d2d663972a
felixkuo0504 <felixkuo0504@gmail.com>
2025-03-02 15:30:14 +0800

    style:refactor PreferencesClass formatting for consistency

--- src/main/java/frc/robot/PreferencesClass.java
+++ src/main/java/frc/robot/PreferencesClass.java
@@ -8,47 +8,47 @@ import java.util.Map;
 
 /** Add your docs here. */
 public class PreferencesClass {
-    public class DriveMotorInverted {
-        public static Map<String, Boolean> AUDriveMotorInverted_MAP = Map.of(
-                "kFrontLeftDriveMotorInverted", false,
-                "kFrontRightDriveMotorInverted", true,
-                "kBackLeftDriveMotorInverted", false,
-                "kBackRightDriveMotorInverted", true);
+  public class DriveMotorInverted {
+    public static Map<String, Boolean> AUDriveMotorInverted_MAP = Map.of(
+        "kFrontLeftDriveMotorInverted", false,
+        "kFrontRightDriveMotorInverted", true,
+        "kBackLeftDriveMotorInverted", false,
+        "kBackRightDriveMotorInverted", true);
 
-        public static Map<String, Boolean> TWNDriveMotorInverted_MAP = Map.of(
-                "kFrontLeftDriveMotorInverted", true,
-                "kFrontRightDriveMotorInverted", false,
-                "kBackLeftDriveMotorInverted", true,
-                "kBackRightDriveMotorInverted", false);
-        public static Map<String, Boolean> currentConfig = AUDriveMotorInverted_MAP;
+    public static Map<String, Boolean> TWNDriveMotorInverted_MAP = Map.of(
+        "kFrontLeftDriveMotorInverted", true,
+        "kFrontRightDriveMotorInverted", false,
+        "kBackLeftDriveMotorInverted", true,
+        "kBackRightDriveMotorInverted", false);
+    public static Map<String, Boolean> currentConfig = AUDriveMotorInverted_MAP;
 
-        public static boolean get(String key) {
+    public static boolean get(String key) {
 
-            return currentConfig.get(key);
-        }
-
+      return currentConfig.get(key);
     }
 
-    public class CanCoderMagOffset {
-        public static Map<String, Double> AUCanCoderMagOffset_MAP = Map.of(
-                "kFrontLeftCanCoderMagOffset", 0.069092,
-                "kFrontRightCanCoderMagOffset", 0.369141,
-                "kBackLeftCanCoderMagOffset", 0.401855,
-                "kBackRightCanCoderMagOffset", -0.010254);
-        public static Map<String, Double> TWNCanCoderMagOffset_MAP = Map.of(
-                "kFrontLeftCanCoderMagOffset", -0.031738,
-                "kFrontRightCanCoderMagOffset", -0.409668,
-                "kBackLeftCanCoderMagOffset", 0.180908,
-                "kBackRightCanCoderMagOffset", 0.141846
+  }
 
-        );
-        public static Map<String, Double> currentConfig = AUCanCoderMagOffset_MAP;
+  public class CanCoderMagOffset {
+    public static Map<String, Double> AUCanCoderMagOffset_MAP = Map.of(
+        "kFrontLeftCanCoderMagOffset", 0.069092,
+        "kFrontRightCanCoderMagOffset", 0.369141,
+        "kBackLeftCanCoderMagOffset", 0.401855,
+        "kBackRightCanCoderMagOffset", -0.010254);
+    public static Map<String, Double> TWNCanCoderMagOffset_MAP = Map.of(
+        "kFrontLeftCanCoderMagOffset", -0.031738,
+        "kFrontRightCanCoderMagOffset", -0.409668,
+        "kBackLeftCanCoderMagOffset", 0.180908,
+        "kBackRightCanCoderMagOffset", 0.141846
 
-        public static double get(String key) {
+    );
+    public static Map<String, Double> currentConfig = AUCanCoderMagOffset_MAP;
 
-            return currentConfig.get(key);
-        }
+    public static double get(String key) {
 
+      return currentConfig.get(key);
     }
+
+  }
 
 }
