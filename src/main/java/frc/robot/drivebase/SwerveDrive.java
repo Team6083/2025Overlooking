90dc75f402be67a1e204783a6a3b0f4f6bc5189f
felixkuo0504 <felixkuo0504@gmail.com>
2025-03-02 15:11:45 +0800

    style:swervedrive

--- src/main/java/frc/robot/drivebase/SwerveDrive.java
+++ src/main/java/frc/robot/drivebase/SwerveDrive.java
@@ -24,15 +24,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveBaseConstant;
 import frc.robot.PreferencesClass;
 import frc.robot.PreferencesClass.CanCoderMagOffset;
 import frc.robot.PreferencesClass.DriveMotorInverted;
-import frc.robot.Constants.DriveBaseConstant;
-
+import org.json.simple.parser.ParseException;
 import java.io.IOException;
 
 
-import org.json.simple.parser.ParseException;
+
 
 public class SwerveDrive extends SubsystemBase {
   private final SwerveModule frontLeft;
