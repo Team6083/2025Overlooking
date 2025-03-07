c04df15abfaa2a7876f832c0d16ff87013ac97a7
BrianHu0925 <j100376@my.cmsh.cyc.edu.tw>
2025-03-07 10:55:48 +0800

    feat: refine precision thresholds in SwerveToTagCmd for improved targeting accuracy

--- src/main/java/frc/robot/commands/SwerveToTagCmd.java
+++ src/main/java/frc/robot/commands/SwerveToTagCmd.java
@@ -75,6 +75,6 @@ public class SwerveToTagCmd extends Command {
   @Override
   public boolean isFinished() {
     return tagTracking.getTv() == 0
-    || (Math.abs(txController.getError()) < 0.05 && Math.abs(tzController.getError()) < 0.05);
+    || (Math.abs(txController.getError()) < 0.03 && Math.abs(tzController.getError()) < 0.03);
   }
 }
