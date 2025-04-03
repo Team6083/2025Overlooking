b4994d1fa782ea6c05d8b190fd1e4c6dc8adb8c5
Hannahjjj97 <hannahjjj97@gmail.com>
2025-04-03 14:42:03 +0800

    style: add gyro connection and calibration status to SmartDashboard

--- src/main/java/frc/robot/drivebase/SwerveDrive.java
+++ src/main/java/frc/robot/drivebase/SwerveDrive.java
@@ -302,6 +302,9 @@ public class SwerveDrive extends SubsystemBase {
     SmartDashboard.putNumber("PoseY", getPose2d().getY());
     SmartDashboard.putNumber("PoseRotationDegree",
         getPose2d().getRotation().getDegrees());
+    SmartDashboard.putBoolean("GyroIsConnected", gyro.isConnected());
+    SmartDashboard.putBoolean("GyroIsCalibrating", gyro.isCalibrating());
+
     Preferences.initInt("WhereCanCoderMagOffset", whereCanCoderMagOffset);
     trueCanCoderMagOffset = Preferences.getInt("WhereCanCoderMagOffset", whereCanCoderMagOffset);
     if (trueCanCoderMagOffset != Preferences.getInt("WhereCanCoderMagOffset", whereCanCoderMagOffset)) {
