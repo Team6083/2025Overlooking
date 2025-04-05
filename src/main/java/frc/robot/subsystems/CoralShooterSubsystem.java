0d8ee37c9e1996c864e2f80bac5dbb27d0ab30cc
Hannahjjj97 <hannahjjj97@gmail.com>
2025-04-06 00:19:27 +0800

    refactor: reorder import statements in CoralShooterSubsystem for consistency

--- src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -11,8 +11,8 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.CoralShooterConstant;
 import frc.robot.ConfigChooser;
+import frc.robot.Constants.CoralShooterConstant;
 import frc.robot.lib.sensor.distance.Rev2mDistanceSensor;
 
 public class CoralShooterSubsystem extends SubsystemBase {
