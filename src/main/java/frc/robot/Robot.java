4bff9f665aeb538c536c8c5421dc1399588b85a3
Hannahjjj97 <hannahjjj97@gmail.com>
2025-04-07 23:45:14 +0800

    fix: clean up imports in Robot class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,6 @@
 
 package frc.robot;
 
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.CvSink;
 import edu.wpi.first.cscore.CvSource;
@@ -19,6 +16,8 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
 
 public class Robot extends TimedRobot {
   // CHECKSTYLE.OFF: MemberName
@@ -35,6 +34,7 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+
     CameraServer.startAutomaticCapture();
     camera = CameraServer.startAutomaticCapture();
     camera.setResolution(640, 480);
