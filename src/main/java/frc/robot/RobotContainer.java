3880b7ba05b944f202655f9adb64b5f802e1eb08
BrianHu0925 <j100376@my.cmsh.cyc.edu.tw>
2025-04-07 19:03:40 +0800

    fix: update coral shooter timeout to use configurable value from ConfigChooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -109,7 +109,7 @@ public class RobotContainer {
     mainController.rightBumper().whileTrue(coralShooterSubsystem.coralShooterOutCmd());
     mainController.rightBumper().and(mainController.leftBumper())
         .toggleOnTrue(new SequentialCommandGroup(new CoralShooterInWithAutoStopCmd(coralShooterSubsystem),
-            coralShooterSubsystem.coralShooterInCmd().withTimeout(0.25)));
+            coralShooterSubsystem.coralShooterInCmd().withTimeout(ConfigChooser.CoralShooter.getDouble("kCoralInTimeOut"))));
     mainController.button(10).whileTrue(coralShooterSubsystem.coralShooterReverseShootCmd());
 
     // Elevator
