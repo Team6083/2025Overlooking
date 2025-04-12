cad2c1e4c93bf719b090497bb149dfed17f47c09
Hannahjjj97 <hannahjjj97@gmail.com>
2025-04-12 08:58:33 +0800

    refactor: clean up auto path definitions and add new auto folder

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.drivebase.SwerveDrive;
 
 public class Robot extends TimedRobot {
   // CHECKSTYLE.OFF: MemberName
