fb988ff0b3930102a293dd01f3504b4fcaa76706
Dinner son <zsu963064@gmail.com>
2025-08-21 09:04:55 +0800

    fix: adjust coral shooter input speed and clean up method formatting

--- src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralShooterSubsystem.java
@@ -43,7 +43,8 @@ public class CoralShooterSubsystem extends SubsystemBase {
     coralShooterMotor.set(ControlMode.PercentOutput, speed);
   }
 
-  public void coralShooterIn() {
+  public void 
+  coralShooterIn() {
     setMotorSpeed(ConfigChooser.CoralShooter.getDouble("kCoralInSpeed"));
   }
 
